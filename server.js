69560abda27ea204c56fa32aea74e781759adf6f
vineetvegi <vineetvegi@gmail.com>
2020-08-19 16:36:13 -0400

    still not working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/Beer-Api'));
+app.use(express.static(__dirname + '/dist/Beer-Api'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname));
 });
