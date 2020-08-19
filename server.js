5c1fc5f8fa7a0f147a37fd4a460d959159c2c154
vineetvegi <vineetvegi@gmail.com>
2020-08-19 16:09:11 -0400

    forgot to build

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/Beer-DB'));
+app.use(express.static(__dirname + '/dist/Beer-Api'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Beer-Api/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
