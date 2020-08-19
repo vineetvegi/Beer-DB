c2743a731f9bef8d55a83d7b104c50748d8b5c0a
vineetvegi <vineetvegi@gmail.com>
2020-08-19 16:31:02 -0400

    problems with hosting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/Beer-Api'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname, 'Beer-Api', 'index.html'));
+res.sendFile(path.join(__dirname));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
