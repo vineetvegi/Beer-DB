7fae6f5601336ab2fd645e5bb2ba27e7ea425d1e
vineetvegi <vineetvegi@gmail.com>
2020-08-19 17:48:09 -0400

    fixed express route

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Beer-Api'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname));
+res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+const server = http.createServer(app);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log("Listening on port 8080");
