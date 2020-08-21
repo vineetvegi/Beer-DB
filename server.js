6e4342f64f60336c89611514bf6a60c58a60f47f
vineetvegi <vineetvegi@gmail.com>
2020-08-20 22:15:13 -0400

    second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/Beer-api'));
-app.get('*', function(req,res)
+app.get('/*', function(req,res)
 res.sendFile(path.join(__dirname + '/dist/Beer-api/index.html'));
 });
 // Start the app by listening on the default Heroku port
